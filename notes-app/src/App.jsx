83acc1c438a0e3caf65eddecd9fce893d120e1e6
Kifayat Yaseen Bhat <kifayatyaseenbhat@gmail.com>
2026-06-29 12:38:54 +0530

    Day 20: Notes App complete - search feature with filter + includes

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,43 +1,59 @@
 import { useState } from "react";
 
-function App(){
- const[inputText, setInputText] = useState("")
- const[noteText, setNoteText] = useState([]);
- function handleClick(){
-  if(!inputText.trim()){
-        return;
-  }
-  else{
-      setNoteText([...noteText,{id:Date.now(), noteText : inputText}])
+function App() {
+
+  const [inputText, setInputText] = useState("")
+  const [noteText, setNoteText] = useState([]);
+  const [search, setSearch] = useState("");
+
+  function handleClick() {
+    if (!inputText.trim()) {
+      return;
+    }
+    else {
+      setNoteText([...noteText, { id: Date.now(), noteText: inputText }])
       setInputText("");
+    }
   }
- }
- function handleDelete(id){
-      setNoteText(noteText.filter((note)=>{
-        return note.id !== id;
-      }))
- }
+  function handleDelete(id) {
+    setNoteText(noteText.filter((note) => {
+      return note.id !== id;
+    }))
+  }
+
+  const filteredNotes = noteText.filter((note)=>{
+    return note.noteText.toLowerCase().includes(search.toLowerCase());
+  }
+
+  )
 
   return (
     <div>
-<input type="text" value = {inputText} onChange={(e)=>{
-              setInputText(e.target.value);
-              
-}} />
-    <button onClick={handleClick}>
-                  Add Note</button>
+      <input placeholder="Enter Note" type="text" value={inputText} onChange={(e) => {
+        setInputText(e.target.value);
+
+      }} />
+
+      <button onClick={handleClick}>
+        Add Note</button>
+
+      <input type="text" placeholder="Search Note" value={search} onChange={(e) => {
+        setSearch(e.target.value);
+      }
+      } />
       <ul>
         {
-          noteText.map((note)=>{
-            return <li 
-            key = {note.id}>
+          filteredNotes.map((note) => {
+            return <li
+              key={note.id}>
               {note.noteText}
-              <button onClick={()=>{handleDelete(note.id)}} >Delete</button>
-            
+              <button onClick={() => { handleDelete(note.id) }} >Delete</button>
+
             </li>
           })
         }
       </ul>
+      { search && !filteredNotes.length && <p>No notes found</p>}
 
     </div>
   )
